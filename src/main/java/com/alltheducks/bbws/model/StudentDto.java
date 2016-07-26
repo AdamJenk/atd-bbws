da6a2607d3b4f723a7e262d98b443cb49b516a42
Adam Jenkins <adamdgjenkins18@gmail.com>
2016-07-26 11:45:52 +0100

    Student Data Struct

---
+++ src/main/java/com/alltheducks/bbws/model/StudentDto.java
@@ -0,0 +1,61 @@
+package com.alltheducks.bbws.model;
+
+
+public class StudentDto {
+    private String id;
+    private String uun;
+    private String familyName;
+    private String middleName;
+    private String givenName;
+
+    //Something for relationships
+	
+	public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUun() {
+        return uun;
+    }
+
+    public void setCourseId(String uun) {
+        this.uun = uun;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+/*
+    public List<CourseDto> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<CourseDto> children) {
+        this.children = children;
+    }
+*/
+}
